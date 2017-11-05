b5f98876786a8d529f53067f72e63863717a06c3
icks <pfaff.felix@gmail.com>
2017-11-05 21:11:03 +0100

    Updated requirements in setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name='tagesschau',
     version='1.0',
-    packages=['requests'],
     url='https://github.com/handflucht/tagesschauDownloader',
     license='',
     author='handflucht',
     author_email='',
-    description='Downloader for Tagesschau, Tagesthemen etc.'
+    description='Downloader for Tagesschau, Tagesthemen etc.',
+    requires=['requests']
 )
